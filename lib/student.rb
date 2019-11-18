2ab976f5e8b0b1dc2f20e9088766ba84bba3e4a1
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 19:53:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
   
   def hello
-    "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff.\n"
   end
   
   def raise_hand
-    "Pick me!\n"
+    puts "Pick me!\n"
   end
 end
