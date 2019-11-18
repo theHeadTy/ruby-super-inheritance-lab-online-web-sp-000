cacee70ae889d63c74bcf30490d644d7a0cb0a6e
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 19:51:44 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,9 @@
 class ChattyStudent
   def hello
-    <<~HEREDOC
       "Hey there! I'm so excited to learn stuff.\n
       How are you doing today? I'm okay, but I'm kind of tired. 
       Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! 
       What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-    HEREDOC
   end
   
   def raise_hand
