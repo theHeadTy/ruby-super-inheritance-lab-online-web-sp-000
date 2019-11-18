eaab7b168256e07c13649fd423c33d195d19682b
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 19:47:38 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,9 @@ class ChattyStudent
       What, you don't want any spoilers? Okay well let me just tell you who died...\n"
     HEREDOC
   end
+  
+  def raise_hand
+    10.times { "Pick me!\n" }
 
 
 describe "ChattyStudent" do 
